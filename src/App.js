f771955f0c35700bb741c536f9991f69803789be
Matheusdamaceno <damaceno.martins@hotmail.com>
2021-03-25 20:00:21 +0000

    todolist

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function Todo({ todo }) {
+    return (
+        <div className="todo">
+            {todo.text}
+        </div>
+    );
+};
+
+function App() {
+    const [todos, setTodos] = React.useState([
+        { text: "Fazer as Atividades da Quinzena" },
+        { text: "Estudar para PW3" },
+        { text: "Marcar Reunião para o TCC" },
+        { text: "Realizar os exercicios" },
+    ]);
+    return (
+        <div className="app">
+            <div className="todo-list">
+                {todos.map((todo, index) =>(
+                    <Todo
+                    key={index}
+                    index={index}
+                    todo={todo}
+                  />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default App;
